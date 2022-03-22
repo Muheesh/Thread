243ddc0e2b6b18ba1eb25efa02580705241c48f4
Muheesh <muheeshas@gmail.com>
2022-03-22 15:10:58 +0530

    Thread

---
+++ src/com/harman/project/trignometry.java
@@ -0,0 +1,71 @@
+package com.harman.project;
+
+import java.util.Scanner;
+
+class Sine_value extends Thread {
+    double a;
+
+    public Sine_value(double a) {
+        this.a = a;
+    }
+
+    @Override
+    public void run() {
+        try {double b,c;
+             b = Math.toRadians(a);
+             c = Math.sin(b);
+            System.out.println("The sine value is : " + c);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+class Cosine_value extends Thread{
+    double a;
+    public Cosine_value(double a) {
+        this.a = a;
+    }
+
+    @Override
+    public void run() {
+        try{
+            double b,c;
+            b = Math.toRadians(a);
+            c = Math.cos(b);
+            System.out.println("The sine value is : "+c);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
+class Tangent_value extends Thread{
+    double a;
+    public Tangent_value(double a) {
+        this.a = a;
+    }
+    @Override
+    public void run() {
+        try{double b,c;
+            b = Math.toRadians(a);
+            c = Math.tan(b);
+            System.out.println("The sine value is : "+c);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
+public class trignometry {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double x;
+        System.out.println("Enter the degree");
+        x = input.nextInt();
+        Sine_value sin = new Sine_value(x);
+        Cosine_value cos = new Cosine_value(x);
+        Tangent_value tan = new Tangent_value(x);
+        sin.start();
+        cos.start();
+        tan.start();
+
+    }
+}
